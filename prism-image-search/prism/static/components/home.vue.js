21fb990eeb5bab7b918b1c7ff41d3fb7d0e0f846
Jesse Schmidt <jschmidt@aerospike.com>
2024-04-04 14:44:47 -0700

    replace old logo with new logo. Remove "Proximus"

--- prism-image-search/prism/static/components/home.vue.js
+++ prism-image-search/prism/static/components/home.vue.js
@@ -1,11 +1,11 @@
 const Home = {
-    template: `
+  template: `
     <div>
-        <h1 class="title">Proximus Image search</h1>
+        <h1 class="title">Aerospike Vector Search</h1>
         <div class="p-3 align-items-center justify-content-center">
         <p>
           <div class="d-flex flex-column flex-lg-row align-items-md-stretch justify-content-md-center gap-3 mb-4">
-                <img class="proximus img-responsive center-block" src="/images/proximus.png" alt="Proximus"/>
+                <img class="proximus img-responsive center-block" src="/images/proximus.png"/>
           </div>
         </p>
 
@@ -14,7 +14,7 @@ const Home = {
         by indexing them using the <a class="link-dark" href="https://huggingface.co/sentence-transformers/clip-ViT-B-32-multilingual-v1">CLIP</a>
         model created by OpenAI. This model generates vectors with semantic meaning 
         from each image and stores them as vector embeddings in Aerospike. When a user
-        submits a query, Proximus generates a vector embedding for the provided text
+        submits a query, this demo app generates a vector embedding for the provided text
         and performs an Approximate Nearest Neighbor (<a class="link-dark"
         href="https://en.wikipedia.org/wiki/Nearest_neighbor_search#Approximate_nearest_neighbor">ANN</a>) 
         search using the Hierarchical Navigable Small World (<a class="link-dark"
