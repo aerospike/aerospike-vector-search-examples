5effcd0b5df43e53a2f4ed0b1975332351f06851
Joe Martin <jmartin@aerospike.com>
2024-09-10 17:42:14 +0530

    update helm chart version

--- kubernetes/full-create-and-install.sh
+++ kubernetes/full-create-and-install.sh
@@ -387,7 +387,7 @@ deploy_avs_helm_chart() {
     helm repo add aerospike-helm https://artifact.aerospike.io/artifactory/api/helm/aerospike-helm
     helm repo update
     if [ -z "$CHART_LOCATION" ]; then
-        helm install avs-gke --values "manifests/avs-gke-values.yaml" --namespace avs aerospike-helm/aerospike-vector-search --version 0.4.0 --wait
+        helm install avs-gke --values "manifests/avs-gke-values.yaml" --namespace avs aerospike-helm/aerospike-vector-search --version 0.4.1 --wait
     else
         helm install avs-gke --values "manifests/avs-gke-values.yaml" --namespace avs "$CHART_LOCATION" --wait
     fi
