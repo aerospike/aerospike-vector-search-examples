49de4073e66e9e1c98b324902a1e3ef65cec0d5f
Adam Hevenor <ahevenor@aerospike.com>
2024-12-09 08:48:56 -0700

    index-update

--- kubernetes/full-create-and-install-gke.sh
+++ kubernetes/full-create-and-install-gke.sh
@@ -444,10 +444,10 @@ deploy_avs_helm_chart() {
         --namespace avs\
         --version $CHART_VERSION\
         --atomic --wait
-# Install AVS update node
+# Install AVS index-update node
     helm install avs-app-update aerospike-helm/aerospike-vector-search\
         --set replicaCount=1 \
-        --set aerospikeVectorSearchConfig.cluster.node-roles[0]=INDEX_UPDATE \
+        --set aerospikeVectorSearchConfig.cluster.node-roles[0]=index-update \
         --values $BUILD_DIR/manifests/avs-values.yaml \
         --namespace avs\
         --version $CHART_VERSION\
